84acb768b5db487be7ef4cca952769778f2dc75a
coderkisoo <coderkisoo@gmail.com>
2017-09-20 09:54:37 +0800

    Initial commit

---
+++ app/src/main/java/com/example/shs1329/viewtest/CustomView.java
@@ -0,0 +1,41 @@
+package com.example.shs1329.viewtest;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by shs1329 on 2017/9/4.
+ */
+
+public class CustomView extends View {
+    public CustomView(Context context) {
+        super(context);
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Paint paint = new Paint();
+        paint.setColor(Color.RED);
+        canvas.drawCircle(100,100,70,paint);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+    }
+}
